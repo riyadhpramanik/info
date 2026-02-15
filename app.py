63114ef8f7eeb94ea2b3b54e9bc15fc3b931b00f
riyadhpramanik <riyadhpramanik38@gmail.com>
2026-02-15 22:28:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def get_jwt_token_sync(region):
         "US": "https://jwt-token-api-by-ajay.vercel.app/token?uid=4422013059&password=9A14867BBA0091781F5BBAC54DDC945B7C3B317B5C35E01AA62BC67DD910F22F",
         "SAC": "https://jwt-token-api-by-ajay.vercel.app/token?uid=4422013059&password=9A14867BBA0091781F5BBAC54DDC945B7C3B317B5C35E01AA62BC67DD910F22F",
         "NA": "https://jwt-token-api-by-ajay.vercel.app/token?uid=4422013059&password=9A14867BBA0091781F5BBAC54DDC945B7C3B317B5C35E01AA62BC67DD910F22F",
-        "default": "https://jwt-token-api-by-ajay.vercel.app/token?uid=4422013059&password=9A14867BBA0091781F5BBAC54DDC945B7C3B317B5C35E01AA62BC67DD910F22F"
+        "default": "https://teamcrbapi1.vercel.app/api/token?uid=4422013059&password=9A14867BBA0091781F5BBAC54DDC945B7C3B317B5C35E01AA62BC67DD910F22F"
     }    
     url = endpoints.get(region, endpoints["default"])
     with jwt_lock:
